e192fc04d3b3d682d012a35db0013b73af76462f
Caleb R <cjr4912@gmail.com>
2020-08-10 21:37:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,13 @@
 class Dog 
-  def initialize(.name=)
+  def initialize(name=)
     @name= name 
   end
   
-  def .name
+  def name
     @name
   end
   
-  def 
+  def bark 
+    puts "Woof!"
+  end
     
